ce353075a214ef9adf02fc4818f71f43489c5b62
prucinski <p.rucinski.19@abdn.ac.uk>
2024-03-19 21:47:52 +0000

    starting a function skeleton for calculations related to thermal expansion strain

---
+++ getLambdaTerm.m
@@ -0,0 +1,8 @@
+%h - host structure, f - fiber, p - protective coating, a - adhesive
+function lambdaTerm = getLambdaTerm(a) 
+    r_p = a;
+    r_f = a;
+    E_h = a;
+    E_f = a;
+    gap = 0.2;
+    lambdaTerm = a; 
